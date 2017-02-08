489a464c6244c238de8aea95381a643d5cffde27
icatat <ionescuc@dickinson.edu>
2017-02-07 23:05:23 -0500

    Design Improvements Made

---
+++ Cata-Teo.js
@@ -0,0 +1,16 @@
+$(document).off('.data-api');
+// window.onload = function (){
+// $("#first").fadeIn("slow");
+// }
+$(document).ready = function () {
+
+    $("#first").click(start());
+}
+
+function start() {
+  $("#header").show();
+  $("#first").hide();
+
+
+
+}
