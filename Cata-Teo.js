b4fe33af47bf7c47f61e6871c2c493cc5cfc8228
icatat <ionescuc@dickinson.edu>
2017-02-08 19:03:04 -0500

    Navigation bar added and edited

--- Cata-Teo.js
+++ Cata-Teo.js
@@ -2,15 +2,13 @@ $(document).off('.data-api');
 // window.onload = function (){
 // $("#first").fadeIn("slow");
 // }
-$(document).ready = function () {
-
-    $("#first").click(start());
-}
+// $(document).ready = function () {
+//     $("#first").click(start());
+//     $("#about").click(about());
+// }
 
 function start() {
   $("#header").show();
   $("#first").hide();
-
-
 
 }
